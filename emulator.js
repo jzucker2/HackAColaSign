640cd04ed44934d9ccc85860b14230dacf88944f
Cody Massin <keyosk@gmail.com>
2016-08-14 19:49:33 -0700

    First pass at the emulator, give it a whirl
    Signed-off-by: Cody Massin <keyosk@gmail.com>

--- emulator.js
+++ emulator.js
@@ -1,3 +1,10 @@
+/*
+
+The following section defines the Constants.h file
+
+*/
+
+
 // PIN Constants
 var HackH = 48;
 var HackA = 26;
@@ -65,6 +72,19 @@ normal = 800;
 ish = 2000;
 extended = 4000;
 
+// Constants
+var PATTERN_OFF = 0; // HIGH
+var PATTERN_ON = 1; // LOW
+
+
+/*
+
+
+The following section defines global methods needed
+
+
+*/
+
 var delay = function(time) {
     var entryTime = Date.now();
     while (true) {
@@ -87,12 +107,17 @@ var random = function(min, max) {
     return Math.floor(Math.random() * ((max - 0) - min) + min);
 }
 
-var PATTERN_OFF = 0; // HIGH
-var PATTERN_ON = 1; // LOW
+/*
+
+
+The following section attempts, transpile, and run the arduino code
+    
+
+*/
 
 var fs = require('fs');
 
-var arduinoCode = fs.readFileSync('./CocaColaAmalgamation/CocaColaAmalgamation.ino').toString();
+var arduinoCode = fs.readFileSync('./CocaCola/CocaCola.ino').toString();
 
 console.log(`
 Original Code: 
@@ -104,14 +129,20 @@ ${arduinoCode}
 var arduinoCodeParts = arduinoCode.split("// )'(");
 
 var runableCode = arduinoCodeParts[1]
-    .replace(/int /g, ' var ')
-    .replace(/void loop\(\) {/, 'while (true) {')
-    .replace(/void ([A-Za-z^)]*)/g, ' var $1 = function');
+    .replace(/int ([A-Za-z]*) =/g, ' var $1 =') // Replace any instance of "int variable = n"
+    .replace(/int /g, '') // Remove "int" from any instance of "int variable"
+    .replace(/void loop\(\) {/, 'while (true) {') // Turn our main method into a while loop
+    .replace(/void ([A-Za-z]*)/g, ' var $1 = function'); // Replace any "void function" with a javascript function definition
 
 console.log(`
 Runable Code: 
 ######################################
 ${runableCode}
+######################################
+`);
+
+console.log(`
+Running code:
 ######################################
 `);
 
