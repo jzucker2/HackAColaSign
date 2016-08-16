2ad86fae1c5b7c719da798336a9a8a290641dd1b
Zachary Drayer <zacharydrayer@gmail.com>
2016-08-16 00:12:21 -0700

    document some things a bit

--- CocaCola/Constants.h
+++ CocaCola/Constants.h
@@ -1,3 +1,5 @@
+// you can pass one of these enum values into `digitalWrite()` as a pin, instead of having to know the raw pin value
+// eg: `pinMode(HackH, PATTERN_INITIALIZE); digitalWrite(HackH, PATTERN_ON);` will turn the letter `H` on
 typedef enum {
     HackH = 48,
     HackA = 26,
@@ -16,7 +18,14 @@ typedef enum {
     RedFive = 32
  } PatternToPin;
 
-static int Lights[16] = {
+// since you can't enumerate over an enum in C, enumerate over this instead, using `sizeof(Lights)` to know how many lights there are
+//    eg:
+//    for (int i = 0; i < sizeof(Lights); i++) {
+//        pinMode(Lights[i], PATTERN_INITIALIZE);
+//        digitalWrite(Lights[i], PATTERN_ON);
+//    }
+//    will turn on everything at once
+static int Lights[] = {
     HackH,
     HackA,
     HackC,
@@ -34,6 +43,7 @@ static int Lights[16] = {
     RedFive
 };
 
+// see above, but, enumerate over this to go through the letters on the sign from left to right
 int letters[] = {
     HackH,
     HackA,
@@ -46,6 +56,7 @@ int letters[] = {
     ColaA,
 };
 
+// also see above, but, enumerate over this to go through the background pieces of the sign from left to right
 int background[] = {
     RedZero,
     RedOne,
@@ -59,13 +70,23 @@ int background[] = {
 #define PATTERN_ON LOW
 #define PATTERN_INITIALIZE OUTPUT
 
+// shared delay intervals, in milliseconds. these values are fairly arbitrary, and you don't have to use them, but, it's probably good to be consistent.
+//    eg:
+//    for (int i = 0; i < sizeof(Lights); i++) {
+//        pinMode(Lights[i], PATTERN_INITIALIZE);
+//        digitalWrite(Lights[i], PATTERN_ON);
+//        delay(normal);
+//    }
+//    will turn on everything, with a delay of 0.8 seconds between each piece turning on
 typedef enum {
     none = 0,
-    blinky = 1000,
     minimal = 500,
     normal = 800,
+    blinky = 1000,
     ish = 2000,
     extended = 4000
 } interval;
 
+// defined for the emulator, doesn't do anything when run otherwise
 #define log(x, ...)
+
--- README.md
+++ README.md
@@ -4,8 +4,11 @@
 - copy the contents of `EverythingOn` into a new folder, and rename `EverythingOn.ino` to folder-name.ino
 
 ## To work with the pattern that is installed on the sign
-- To update the pattern the sign use on the playa, open CocaCola/CocaCola.ino with your preferred editor, modify things, and run the emulator
+- To update the pattern the sign use on the playa, open `CocaCola/CocaCola.ino` with your preferred editor, modify things, and run the emulator
 
 ## To run the emulator
 - With Node 6.3.1: installed, run `node emulator.js`
 - Without Node 6.3.1 installed: Install Node 6.3.1, and see prior instructions
+
+## For further documentation
+- See `CocaCola/Constants.h` 
