b7cc8de38e7d94e7b8faa797d685d5c3007f0665
Zachary Drayer <zacharydrayer@gmail.com>
2017-09-04 12:56:44 -0700

    2017 update

--- EverythingOn/EverythingOn.ino
+++ EverythingOn/EverythingOn.ino
@@ -1,10 +1,23 @@
 #include "Constants.h"
 
 void setup() {
-    for (int i = 0; i < sizeof(Lights); i++) {
-        pinMode(Lights[i], PATTERN_INITIALIZE);
-    }
-}
+      pinMode(Lights[0], PATTERN_INITIALIZE);
+      pinMode(Lights[1], PATTERN_INITIALIZE);
+      pinMode(Lights[2], PATTERN_INITIALIZE);
+      pinMode(Lights[3], PATTERN_INITIALIZE);
+      pinMode(Lights[4], PATTERN_INITIALIZE);
+      pinMode(Lights[5], PATTERN_INITIALIZE);
+      pinMode(Lights[6], PATTERN_INITIALIZE);
+      pinMode(Lights[7], PATTERN_INITIALIZE);
+      pinMode(Lights[8], PATTERN_INITIALIZE);
+      pinMode(Lights[9], PATTERN_INITIALIZE);
+      pinMode(Lights[10], PATTERN_INITIALIZE);
+      pinMode(Lights[11], PATTERN_INITIALIZE);
+      pinMode(Lights[12], PATTERN_INITIALIZE);
+      pinMode(Lights[13], PATTERN_INITIALIZE);
+      pinMode(Lights[14], PATTERN_INITIALIZE);
+      pinMode(Lights[15], PATTERN_INITIALIZE);
+ }
 
 // code above the man is ignored by the emulator
 // code below it is transpiled into js and run by the emulator
@@ -14,7 +27,20 @@ void setup() {
 // roughly: anything that calls `digitalWrite();` is a scene
 
 void loop() {
-    for (int i = 0; i < sizeof(Lights); i++) {
-      digitalWrite(Lights[i], PATTERN_ON);
-    }
+      digitalWrite(Lights[0], PATTERN_ON);
+      digitalWrite(Lights[1], PATTERN_ON);
+      digitalWrite(Lights[2], PATTERN_ON);
+      digitalWrite(Lights[3], PATTERN_ON);
+      digitalWrite(Lights[4], PATTERN_ON);
+      digitalWrite(Lights[5], PATTERN_ON);
+      digitalWrite(Lights[6], PATTERN_ON);
+      digitalWrite(Lights[7], PATTERN_ON);
+      digitalWrite(Lights[8], PATTERN_ON);
+      digitalWrite(Lights[9], PATTERN_ON);
+      digitalWrite(Lights[10], PATTERN_ON);
+      digitalWrite(Lights[11], PATTERN_ON);
+      digitalWrite(Lights[12], PATTERN_ON);
+      digitalWrite(Lights[13], PATTERN_ON);
+      digitalWrite(Lights[14], PATTERN_ON);
+      digitalWrite(Lights[15], PATTERN_ON);
 }
